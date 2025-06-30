4f0501d6d847b1afe1a3873feea1a4a1fb570f36
Priyank Namdev <priyank.namdev@outlook.com>
2025-06-30 17:43:11 +0100

    Add Azure Keyvault

--- Lab4/main.tf
+++ Lab4/main.tf
@@ -12,7 +12,12 @@ resource "azurerm_key_vault" "example" {
   name                        = "kev-${var.application_name}-${random_string.keyvault_name.result}"
   location                    = azurerm_resource_group.main.location
   resource_group_name         = azurerm_resource_group.main.name
-  tenant_id                   = "ceef7486-1ffe-4642-bbe7-7655fb3e8777" # Replace with your Azure tenant ID
+  tenant_id                   = data.azurerm_client_config.myself.tenant_id # Replace with your Azure tenant ID
   sku_name                    = "standard"
 
+}
+
+data "azurerm_client_config" "myself" {
+  # This data source retrieves the current Azure client configuration
+  # It is used to get the tenant ID and subscription ID for the Key Vault access policy
 }

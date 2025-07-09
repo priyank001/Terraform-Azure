69407f4158b467b492eb72872579de083650e8b9
Priyank Namdev <priyank.namdev@outlook.com>
2025-07-09 15:54:43 +0100

    [Lab4] Add Role Assignment to myself for Key_vault reader access

--- Lab4/main.tf
+++ Lab4/main.tf
@@ -8,16 +8,22 @@ resource "random_string" "keyvault_name" {
   upper   = false
   special = false
 }
+
 resource "azurerm_key_vault" "example" {
   name                        = "kev-${var.application_name}-${random_string.keyvault_name.result}"
   location                    = azurerm_resource_group.main.location
   resource_group_name         = azurerm_resource_group.main.name
-  tenant_id                   = data.azurerm_client_config.myself.tenant_id # Replace with your Azure tenant ID
+  tenant_id                   = data.azurerm_client_config.myself.tenant_id # Use the tenant ID from the client config
   sku_name                    = "standard"
-
 }
 
 data "azurerm_client_config" "myself" {
   # This data source retrieves the current Azure client configuration
   # It is used to get the tenant ID and subscription ID for the Key Vault access policy
+}
+
+resource "azurerm_role_assignment" "myself" {
+  scope                = azurerm_key_vault.example.id
+  role_definition_name = "Reader"
+  principal_id         = data.azurerm_client_config.myself.object_id # Use the object ID from the client config
 }
